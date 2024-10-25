8f26bba6cf042aac9e106db7b64e8f2b06527691
Zen1971 <olstephnat@gmail.com>
2024-10-25 11:13:47 +0200

    Modification App.java

--- App.java
+++ App.java
@@ -1,3 +1,13 @@
-import pokedex.json.simple.JSONObject;
+import org.json.simple.JSONObject;
+
+public class App{
+	public static void main(string[] args) throws Exception{
+		JSONObject obj = new JSONObject();
+		obj.put("name","Taha");
+		obj.put("birth","Sep-1st");
+		
+		system.out.printIn(obj);
+	}
+}
 
 
